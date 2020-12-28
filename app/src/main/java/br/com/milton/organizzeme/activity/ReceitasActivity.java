984a988a7a8fd8b000cb19e83b13ebca7d552b87
MiltonDevSis <milton.dev.sis@gmail.com>
2020-12-28 15:01:53 -0300

    Interface receitas e despesas, aplicando temas

---
+++ app/src/main/java/br/com/milton/organizzeme/activity/ReceitasActivity.java
@@ -0,0 +1,16 @@
+package br.com.milton.organizzeme.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.milton.organizzeme.R;
+
+public class ReceitasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receitas);
+    }
+}
