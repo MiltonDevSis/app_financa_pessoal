e6f477db1d1d5aa24238b47b3e934f868361bbbb
MiltonDevSis <milton.dev.sis@gmail.com>
2021-01-01 14:06:21 -0300

    Salvando despesa

--- app/src/main/java/br/com/milton/organizzeme/activity/DespesasActivity.java
+++ app/src/main/java/br/com/milton/organizzeme/activity/DespesasActivity.java
@@ -3,17 +3,20 @@ package br.com.milton.organizzeme.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 import br.com.milton.organizzeme.R;
 import br.com.milton.organizzeme.helper.DateCustom;
+import br.com.milton.organizzeme.model.Movimentacao;
 
 public class DespesasActivity extends AppCompatActivity {
 
     private TextInputEditText campoData, campoCategoria, campoDescricao;
     private EditText campoValor;
+    private Movimentacao movimentacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,20 @@ public class DespesasActivity extends AppCompatActivity {
 
         // preenche o campo data com a data atual
         campoData.setText(DateCustom.dataAtual());
+
+    }
+
+    public void salvarDespesa(View view){
+
+        movimentacao = new Movimentacao();
+        String data = campoData.getText().toString();
+        movimentacao.setData( data );
+        movimentacao.setCategoria(campoCategoria.getText().toString());
+        movimentacao.setDescricao(campoDescricao.getText().toString());
+        movimentacao.setValor(Double.parseDouble(campoValor.getText().toString()));
+        movimentacao.setTipo("d");
+
+        movimentacao.salvar(data);
 
     }
 }
---
+++ app/src/main/java/br/com/milton/organizzeme/model/Movimentacao.java
@@ -0,0 +1,80 @@
+package br.com.milton.organizzeme.model;
+
+import android.util.Base64;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+import br.com.milton.organizzeme.config.ConfiguracaoFirebase;
+import br.com.milton.organizzeme.helper.Base64Custom;
+import br.com.milton.organizzeme.helper.DateCustom;
+
+/**
+ * Created by MiltonDevSis on 01/01/2021.
+ */
+public class Movimentacao {
+
+    private String categoria;
+    private String data;
+    private String descricao;
+    private String tipo;
+    private Double valor;
+
+    public Movimentacao() {
+    }
+
+    public void salvar(String dataEscolhida){
+
+        FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        String idUsuario = Base64Custom.codificaBase64( autenticacao.getCurrentUser().getEmail());
+        String mesAno = DateCustom.mesAnoDataEscolhida( dataEscolhida );
+
+        DatabaseReference firebase = ConfiguracaoFirebase.getFirebaseDatabase();
+        firebase.child("movimentacao")
+                .child( idUsuario )
+                .child( mesAno )
+                .push()
+                .setValue(this);
+
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+}
