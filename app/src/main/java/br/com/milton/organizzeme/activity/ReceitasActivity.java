a118dc7bc6d162f46cd7494342898aaff74c580f
MiltonDevSis <milton.dev.sis@gmail.com>
2021-01-01 19:04:48 -0300

    Salvando receitas

--- app/src/main/java/br/com/milton/organizzeme/activity/ReceitasActivity.java
+++ app/src/main/java/br/com/milton/organizzeme/activity/ReceitasActivity.java
@@ -1,16 +1,134 @@
 package br.com.milton.organizzeme.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import br.com.milton.organizzeme.R;
+import br.com.milton.organizzeme.config.ConfiguracaoFirebase;
+import br.com.milton.organizzeme.helper.Base64Custom;
+import br.com.milton.organizzeme.helper.DateCustom;
+import br.com.milton.organizzeme.model.Movimentacao;
+import br.com.milton.organizzeme.model.Usuario;
 
 public class ReceitasActivity extends AppCompatActivity {
+
+    private TextInputEditText campoData, campoCategoria, campoDescricao;
+    private EditText campoValor;
+    private Movimentacao movimentacao;
+    private DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    private Double receitaTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receitas);
+
+        campoValor = findViewById(R.id.edtValor);
+        campoData = findViewById(R.id.edtData);
+        campoCategoria = findViewById(R.id.edtCategoria);
+        campoDescricao = findViewById(R.id.edtDescricao);
+
+        campoData.setText(DateCustom.dataAtual());
+        recuperarReceitaTotal();
+    }
+
+    public void salvarReceita(View view){
+
+        if ( validarCamposReceitas() ){
+
+            movimentacao = new Movimentacao();
+            String data = campoData.getText().toString();
+            Double valorRecuperado = Double.parseDouble(campoValor.getText().toString());
+
+            movimentacao.setData( data );
+            movimentacao.setCategoria(campoCategoria.getText().toString());
+            movimentacao.setDescricao(campoDescricao.getText().toString());
+            movimentacao.setValor( valorRecuperado );
+            movimentacao.setTipo("r");
+
+            Double receitaAtualizada = receitaTotal + valorRecuperado;
+            atualizarReceita( receitaAtualizada );
+
+            movimentacao.salvar(data);
+        }
+    }
+
+    public boolean validarCamposReceitas(){
+
+        String textoValor = campoValor.getText().toString();
+        String textoData = campoData.getText().toString();
+        String textoCategoria = campoCategoria.getText().toString();
+        String textoDescricao = campoDescricao.getText().toString();
+
+        if ( !textoValor.isEmpty()){
+            if ( !textoData.isEmpty()){
+                if ( !textoCategoria.isEmpty()){
+                    if ( !textoDescricao.isEmpty()){
+
+                        return true;
+
+                    }else {
+                        Toast.makeText(ReceitasActivity.this,
+                                "Descrição não foi preenchido", Toast.LENGTH_LONG).show();
+                        return false;
+                    }
+                }else {
+                    Toast.makeText(ReceitasActivity.this,
+                            "Categoria não foi preenchida", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+
+            }else {
+                Toast.makeText(ReceitasActivity.this,
+                        "Data não foi preenchida", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        }else {
+            Toast.makeText(ReceitasActivity.this,
+                    "Valor não foi preenchido", Toast.LENGTH_LONG).show();
+            return false;
+        }
+    }
+
+    public void recuperarReceitaTotal(){
+
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificaBase64(emailUsuario);
+        DatabaseReference usuarioRef = firebaseRef.child("usuarios").child( idUsuario );
+
+        usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                Usuario usuario = snapshot.getValue( Usuario.class );
+                receitaTotal = usuario.getReceitaTotal();
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    public void atualizarReceita(Double receita){
+
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificaBase64(emailUsuario);
+        DatabaseReference usuarioRef = firebaseRef.child("usuarios").child( idUsuario );
+
+        usuarioRef.child("receitaTotal").setValue( receita );
     }
 }
