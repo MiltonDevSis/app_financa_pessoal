d7886ad1f747f0b05b8311b9bf9b02deb9798c40
MiltonDevSis <milton.dev.sis@gmail.com>
2021-01-02 12:04:18 -0300

    Configurando Interface principal

--- app/src/main/java/br/com/milton/organizzeme/activity/PrincipalActivity.java
+++ app/src/main/java/br/com/milton/organizzeme/activity/PrincipalActivity.java
@@ -5,15 +5,19 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 
 import br.com.milton.organizzeme.R;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    private MaterialCalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,9 @@ public class PrincipalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        calendarView = findViewById(R.id.calendarView);
+        configuraCalendarView();
 
        /*FloatingActionButton fab = findViewById(R.id.menu_despesa);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -38,5 +45,15 @@ public class PrincipalActivity extends AppCompatActivity {
 
     public void adicionarDespesa(View view){
         startActivity(new Intent(this, DespesasActivity.class));
+    }
+
+    public void configuraCalendarView(){
+
+        CharSequence meses[]   = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        calendarView.setTitleMonths( meses );
+
+        calendarView.setOnMonthChangedListener((widget, date) -> {
+
+        });
     }
 }
